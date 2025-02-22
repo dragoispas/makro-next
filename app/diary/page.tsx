1858b12e30e8cc6782d3d7d79cd7da25f37929c9
dragoispas <drago.ispas@gmail.com>
2025-02-23 01:23:53 +0200

    Diary page changes

--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -8,7 +8,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import MacroBreakdown from "@/components/macro-breakdown";
 import WeightTracker from "@/components/weight-tracker";
 import { Separator } from "@/components/ui/separator";
-import { ChevronLeft, ChevronRight, Calendar as CalendarIcon } from "lucide-react";
+import { ChevronLeft, ChevronRight, Calendar as CalendarIcon, MailOpen, Plus } from "lucide-react";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
 import { format, isToday, addDays } from "date-fns";
@@ -22,35 +22,45 @@ const DiaryPage = () => {
     return (
         <div className="container mx-auto py-10 flex flex-col gap-4">
 
-            <div className="flex items-center gap-4">
-                <Button variant="ghost" onClick={handlePrevDay}>
-                    <ChevronLeft className="w-6 h-6" />
-                </Button>
+            <div className="flex justify-between items-center">
+                <div className="flex items-center gap-4">
+                    <Button variant="ghost" onClick={handlePrevDay}>
+                        <ChevronLeft className="w-6 h-6" />
+                    </Button>
 
-                <Popover>
-                    <PopoverTrigger asChild>
-                        <div className="cursor-pointer flex flex-col items-center">
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <div className="cursor-pointer flex flex-col items-center">
 
-                            <span className="text-xl font-bold">
-                                {isToday(date) ? 'Today' : format(date, 'dd MMM yyyy')}
-                            </span>
-                        </div>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0">
-                        <Calendar
-                            mode="single"
-                            selected={date}
-                            onSelect={(selectedDate) => {
-                                if (selectedDate) setDate(selectedDate);
-                            }}
-                            initialFocus
-                        />
-                    </PopoverContent>
-                </Popover>
+                                <span className="text-xl font-bold">
+                                    {isToday(date) ? 'Today' : format(date, 'dd MMM yyyy')}
+                                </span>
+                            </div>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0">
+                            <Calendar
+                                mode="single"
+                                selected={date}
+                                onSelect={(selectedDate) => {
+                                    if (selectedDate) setDate(selectedDate);
+                                }}
+                                initialFocus
+                            />
+                        </PopoverContent>
+                    </Popover>
 
-                <Button variant="ghost" onClick={handleNextDay}>
-                    <ChevronRight className="w-6 h-6" />
-                </Button>
+                    <Button variant="ghost" onClick={handleNextDay}>
+                        <ChevronRight className="w-6 h-6" />
+                    </Button>
+                </div>
+                <div className="flex gap-4">
+                    <Button>
+                        <Plus /> Add food to diary
+                    </Button>
+                    <Button>
+                        <Plus /> Add note
+                    </Button>
+                </div>
             </div>
 
             <div className="flex gap-4">
